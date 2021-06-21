1c202828f4a6bbfa25a8530f68f04b49813563a4
DelaramRajaei <delaram.rajaei@yahoo.com>
2021-06-22 02:24:32 +0430

    Add constraints

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# Binary Puzzle
-# Authors: Hedieh Pourghasem 9733015 - Delaram Rajaei 9731084
+"""
+Binary Puzzle
+:author Hedieh Pourghasem 9733015
+:author Delaram Rajaei 9731084
+"""
+from BinaryPuzzle import BinaryPuzzle
+
 
 # Get input
 def getInput():
@@ -13,4 +18,10 @@ def getInput():
 
 if __name__ == '__main__':
     puzzle, n = getInput()
+    # list = [1,1,0,1,0,1,0,1]
+    # binary_puzzle = BinaryPuzzle()
+    # print(binary_puzzle.constraint_equal_strings(list))
+    # print(binary_puzzle.constraint_repetitive_strings(list))
+    # print(binary_puzzle.constraint_unique_strings("row","11000110"))
+    # print(binary_puzzle.constraint_unique_strings("column","11000110"))
 
